5723d1bcb3853b3d78a0deb034b8409da6f0d0ce
Mr-Robot-echo-hello-world <hypophyse8@gmail.com>
2021-03-18 14:17:01 +0100

    du pwd login

--- développement/secure.py
+++ développement/secure.py
@@ -4,7 +4,7 @@ from smtplib import SMTP
 def raporteur(text):
 	serveur=SMTP("smtp.gmail.com",587)
 	serveur.starttls()
-	serveur.login('sspameur@gmail.com','i_hate_ISN')
+	serveur.login('sspameur@gmail.com','*******')
 	msg=text
 	subject="raporteur Game   "+strftime("%a, %d %b %Y %H:%M:%S")
 	body="subject: {}\n\n{}".format(subject,msg)
